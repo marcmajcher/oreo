a541681ca1591769184e80c1123cf0135d69e26f
Marc Majcher <majcher@gmail.com>
2019-01-12 04:10:28 -0600

    oreo

--- oreo.js
+++ oreo.js
@@ -14,40 +14,59 @@ const lefts = {
   Ov: 0,
 };
 
+const tops = {
+  O: {
+    O: 30,
+    Ov: 30,
+    RE: 55,
+  },
+  Ov: {
+    O: 20,
+    Ov: 20,
+    RE: 20,
+  },
+  RE: {
+    O: -5,
+    Ov: 80,
+    RE: 13,
+  },
+};
+
 function validateInput(event) {
   const key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
-  if (!/^[10]$/.test(key)) {
+  if (!/^[EORore]$/.test(key)) {
     event.preventDefault();
     return false;
   }
 }
 
 function updateOreo(event) {
-  const str = event.target.value;
-  document.getElementById('oreo-out').innerText = str.replace(/1/g, 'RE').replace(/0/g, 'O');
-
   const oreo = document.getElementById('oreo');
   while (oreo.firstChild) {
     oreo.removeChild(oreo.firstChild);
   }
 
+  const str = event.target.value.replace(/O/gi, '0').replace(/RE/gi, '1').replace(/[^01]/g, '');
+  event.target.value = str.replace(/0/g, 'O').replace(/1/g, 'RE');
+
   let top = 0;
   for (let i = 0; i < str.length; i++) {
     const image = document.createElement('img');
-    let thing = images[str[i]];
-    if (i !== 0 && i === str.length - 1 && thing === 'O') {
-      thing = 'Ov';
+    let currentLayer = images[str[i]];
+    if (i !== 0 && i === str.length - 1 && currentLayer === 'O') {
+      currentLayer = 'Ov';
     }
-    image.setAttribute('src', `/img/${thing}.png`);
+
+    image.setAttribute('src', `/img/${currentLayer}.png`);
     image.style['z-index'] = str.length - i;
-    image.style.left = `${lefts[thing]}px`;
+    image.style.left = `${lefts[currentLayer]}px`;
     image.style.top = `${top}px`;
-    top += 30;
-    if (images[str[i + 1]] === 'RE') {
-      top += 40;
-    }
     image.classList.add('layer');
     oreo.appendChild(image);
+
+    if (i < str.length - 1) {
+      top += tops[currentLayer][images[str[i + 1]]];
+    }
   }
 }
 
