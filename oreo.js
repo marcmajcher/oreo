f741c093eaae917a46f756619eed37057ff2d2ef
Marc Majcher <majcher@gmail.com>
2020-08-16 21:53:53 -0500

    update

--- oreo.js
+++ oreo.js
@@ -4,8 +4,8 @@
 /* eslint-disable consistent-return */
 
 const images = {
-  0: 'O',
-  1: 'RE',
+  O: 'O',
+  RE: 'RE',
 };
 
 const lefts = {
@@ -32,47 +32,42 @@ const tops = {
   },
 };
 
-function validateInput(event) {
-  const key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
-  if (!/^[EORore]$/.test(key)) {
-    event.preventDefault();
-    return false;
-  }
-}
-
-function updateOreo(event) {
+function updateOreo() {
   const oreo = document.getElementById('oreo');
+  const oreoIn = document.getElementById('oreo-in')
+
   while (oreo.firstChild) {
     oreo.removeChild(oreo.firstChild);
   }
 
-  const str = event.target.value.replace(/O/gi, '0').replace(/RE/gi, '1').replace(/[^01]/g, '');
-  event.target.value = str.replace(/0/g, 'O').replace(/1/g, 'RE');
+  oreoIn.value = oreoIn.value.replace(/[^ore]/ig, '');
+  const tokens = oreoIn.value.match(/o|re/ig).map(e => e.toUpperCase());
 
   let top = 0;
-  for (let i = 0; i < str.length; i++) {
+  for (let i = 0; i < tokens.length; i++) {
     const image = document.createElement('img');
-    let currentLayer = images[str[i]];
-    if (i !== 0 && i === str.length - 1 && currentLayer === 'O') {
+    let currentLayer = images[tokens[i]];
+    if (i !== 0 && i === tokens.length - 1 && currentLayer === 'O') {
       currentLayer = 'Ov';
     }
 
     image.setAttribute('src', `img/${currentLayer}.png`);
-    image.style['z-index'] = str.length - i;
+    image.style['z-index'] = tokens.length - i;
     image.style.left = `${lefts[currentLayer]}px`;
     image.style.top = `${top}px`;
     image.classList.add('layer');
     oreo.appendChild(image);
 
-    if (i < str.length - 1) {
-      top += tops[currentLayer][images[str[i + 1]]];
+    if (i < tokens.length - 1) {
+      top += tops[currentLayer][images[tokens[i + 1]]];
     }
   }
 }
 
 function init() {
-  document.getElementById('oreo-in').addEventListener('keypress', validateInput);
   document.getElementById('oreo-in').addEventListener('keyup', updateOreo);
+  document.getElementById('oreo-in').value = 'OREO'
+  updateOreo()
 }
 
 if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
